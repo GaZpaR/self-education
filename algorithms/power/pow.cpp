5b56ea2dc0766ef8a2ad2c3cc18608f7ea261ee0
GaZpaR <gazpar@sibmail.com>
2016-07-09 09:54:16 +0600

    [+] Added simple algorithm of powering int num

---
+++ algorithms/power/pow.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+template<class T>
+T power(T num, T pow){
+	if(pow==0)
+		return 1;
+	if(pow%2 == 1)
+		return power(num, pow-1)*num;
+	else{
+		T b=power(num, pow/2);
+		return b*b;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int
+	a = atoi(argv[1]),
+	pw = atoi(argv[2]);
+	std::cout<<a<<std::endl;
+	std::cout<<pw<<std::endl;
+	if(pw == NULL) pw = 0;
+	if(a == NULL) a = 0;
+
+	std::cout<<"Result of power is: "<<power<int>(a, pw)<<std::endl;
+
+	std::cout<<"\n Bye bye!!!\n";
+	return 0;
+}
