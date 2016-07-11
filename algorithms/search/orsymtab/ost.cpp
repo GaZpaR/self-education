8c469066c9ce688efc7ef2862c361ae66f5f014c
GaZpaR <gazpar@sibmail.com>
2016-07-11 18:51:36 +0600

    [*] Fixed method of serial search

--- algorithms/search/orsymtab/ost.cpp
+++ algorithms/search/orsymtab/ost.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 
 
-template<class T>
-T power(T num, T pow){
-	if(pow==0)
-		return 1;
-	if(pow%2 == 1)
-		return power(num, pow-1)*num;
-	else{
-		T b=power(num, pow/2);
-		return b*b;
+template<class T, class key>
+class ST{
+private:
+	T nullItem, *st;
+	int N;
+public:
+	ST(int maxN){
+		st = new T[maxN + 1];
+		N=0;
+	};
+
+	int count(){
+		return N;
+	};
+
+	void insert(T x){
+		int i = N++;
+		key v = x.key();
+		while(i>0 && v<st[i-1].key()){
+			st[i] = st[i-1];
+			i--;
+		}
+		st[i] = x;
 	}
-}
+
+	T search(key v){
+		for(int i=0; i<N; i++){
+			if(st[i].key()<v) break;
+			if(v == st[i].key()) return st[i];
+		}
+		return nullItem;
+	}
+
+	T select(int k){
+		return st[k];
+	};
+	void show(std::ostream &os){
+		int i=0;
+		while(i<N) st[i++].show(os);
+	}
+};
+
 
 int main(int argc, char **argv)
 {
@@ -23,7 +54,7 @@ int main(int argc, char **argv)
 	if(pw == (int)NULL) pw = 0;
 	if(a ==(int)NULL) a = 0;
 
-	std::cout<<"Result of power is: "<<power<int>(a, pw)<<std::endl;
+	std::cout<<"Result of power is: "<<std::endl;
 
 	std::cout<<"\n Bye bye!!!\n";
 	return 0;
