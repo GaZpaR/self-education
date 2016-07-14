d5217d3f30edccb6cbd2df6f2f0ed2a33b5d882d
GaZpaR <gazpar@sibmail.com>
2016-07-14 20:01:29 +0600

    [+] Added noexept implementation

--- basis/keywords/kw.cpp
+++ basis/keywords/kw.cpp
@@ -6,6 +6,14 @@ typedef struct
 	mutable int n2;
 } A;
 
+// whether foo is declared noexcept depends on if the expression
+// T() will throw any exceptions
+template <class T>
+  void foo() noexcept(noexcept(T())) {}
+
+void bar() noexcept(true) {}
+void baz() noexcept { throw 42; }  // noexcept is the same as noexcept(true)
+
 int main(int argc, char* argv[])
 {
 	const A a = {.n1 = 5, .n2 = 15};
@@ -14,7 +22,12 @@ int main(int argc, char* argv[])
 
 	a.n2 = 2;// It's all right
 	std::cout<<"a.n2="<<a.n2<<std::endl;
-	
+
+	foo<int>();  // noexcept(noexcept(int())) => noexcept(true), so this is fine
+
+	bar();  // fine
+	baz();  // compiles, but at runtime this calls std::terminate
+
 	std::cout<<"End program. Bye, Bye!"<<std::endl;
 	return 0;
 }
