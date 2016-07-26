f3b041650a61d0e94e8b648b39118103808cdf53
GaZpaR <gazpar@sibmail.com>
2016-07-26 19:01:06 +0700

    [+] Added args and operations parser
    And delete unusefull part of code

--- basis/slcalc/slc.cpp
+++ basis/slcalc/slc.cpp
@@ -23,7 +23,7 @@ bool expcheck(std::string exp){
 		char c = exp[i];
 		if(c < '(' || c > '9' || c == '\''){
 			if(c != ' ') return false;
-		}	
+		}
 	}
 	return true;
 }
@@ -50,11 +50,63 @@ T eval(int a, int b, const char op){
 	}
 };
 
+// Function to evaluate expression without brackets
+std::string evalexpwb(std::string exp){
+
+	std::vector<int> args;
+	std::vector<char> ops;
+
+	std::cout << exp << std::endl;
+	const uint explen = exp.length();
+	for(uint i=0, ps = 0; i<explen; i++){
+		if( exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/'){
+
+			std::string temps1;
+				temps1.assign(exp, ps, i-ps);
+
+			args.push_back(std::stoi(temps1));
+
+			ops.push_back(exp[i]);
+
+			std::string temps2;
+
+			i++;
+			ps = i;
+			for(; i<explen; i++){
+				if( exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/' ){
+					temps2.assign(exp, ps, i-ps);
+
+					ops.push_back(exp[i]);
+					break;
+				}
+				if(i == explen-1){
+					temps2.assign(exp, ps, i-explen-1);
+				}
+		}
+			args.push_back(std::stoi(temps2));
+			ps = i+1;
+		}
+
+	}
+
+	std::cout << "Arguments are: " << std::endl;
+	for(uint i=0; i<args.size(); i++)
+		std::cout << args[i] << ' ';
+	std::cout << std::endl;
+
+	std::cout << "Operations are: " << std::endl;
+	for(uint i=0; i<ops.size(); i++)
+		std::cout << ops[i] << ' ';
+	std::cout << std::endl;
+
+	return exp;
+}
+
 
 std::string evals(std::string exp){
-	
+
 	// We should divide expression to the blocks if there are any brackets
-	std::vector<int> obp, cbp; // obp- "open bracket position", cbp- "close bracket position" 
+	std::vector<int> obp, cbp; // obp- "open bracket position", cbp- "close bracket position"
 	for(uint i=0; i<exp.length(); i++){
 		char c = exp[i];
 		if(c == '(') obp.push_back(i);
@@ -63,43 +115,13 @@ std::string evals(std::string exp){
 	if(obp.size() != cbp.size()) return (std::string)"Expression have uncoupled brackets";
 
 	if(obp.size() > 0 ){
-		
-		
+		return exp;
 	}
 	else{
 		std::cout << "There is no brackets" << std::endl;
+		return evalexpwb(exp);
+		// int rexp = eval<int>(std::stoi(evals(texp1), nullptr, 0), std::stoi(evals(texp2), nullptr, 0)*sign, c);
 	}
-
-	uint i = 0, j = exp.length();
-	char c = ' ';
-	for(i; i < j; i++){
-		c = exp[i];
-		if(c == '+' || c =='-' || c =='*' || c=='/'){
-			std::cout << "Operation is: \"" << c << "\"" << std::endl;
-			std::string texp1;
-				texp1.assign(exp, 0, i);
-			std::cout << texp1 << std::endl;
-			std::string texp2;
-				texp2.assign(exp, i+1, j-1);
-			std::cout << texp2 << std::endl;
-			try{
-				int sign = 1;
-
-				if(c == '-'){
-					sign = -1;
-					c = '+';
-				}	
-				int rexp = eval<int>(std::stoi(evals(texp1), nullptr, 0), std::stoi(evals(texp2), nullptr, 0)*sign, c);
-				std::string res = std::to_string(rexp);
-				return res;
-			}
-			catch(char const *er){
-				std::cout << er << std::endl;
-			}
-		}
-	}
-	//std::cout << exp << std::endl;
-	return exp;
 }
 
 int main(int argc, char **argv){
@@ -116,9 +138,9 @@ int main(int argc, char **argv){
 			if(i > 0) i--;
 		}
 	}
-	
+
 	std::cout << "Evaluating expression without 'space' is: \"" << evalexp << "\"" << std::endl;
-	
+
 	std::cout << "Result is: " << evals(evalexp) << std::endl;
 	std::cout<<"Hello, suk!"<<std::endl;
 	return 0;
