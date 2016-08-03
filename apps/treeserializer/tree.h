a7d71a7e9b7d793ef2993cc3fae3aa1a79a8dfb8
GaZpaR <gazpar@sibmail.com>
2016-08-03 09:41:08 +0700

    [*] Fixed INode class
    Now INode is abstract inerface for all successors

--- apps/treeserializer/tree.h
+++ apps/treeserializer/tree.h
@@ -10,15 +10,26 @@ enum{
 
 class INode{
 public:
-	virtual ~INode();
-	virtual void getContent(INode *n);
-	virtual void setContent(void *p);
-	virtual uint getChildrenQ(INode *n);
-	virtual INode* getParent(INode *n);
-	virtual INode* getChild(uint i);
-	uint nodeType(){return type;};
+	// This func rets void*
+	// which should be casted to user defined type
+	virtual void* getContent() = 0;
 	
-	uint type;
+	// This funcs setting nodes content.
+	// In this case there are 3 types of content
+	virtual void setContent(int) = 0;
+	virtual void setContent(float) = 0;
+	virtual void setContent(std::string) = 0;
+	
+	// Funcs to operate with child Nodes
+	virtual uint getChildrenQ(INode *n) = 0;
+	virtual INode* getChild(uint) = 0;
+	virtual void addChild(INode) = 0;
+
+	// Get link to parent node
+	virtual INode* getParent() = 0;
+
+	// Func to define type of node
+	virtual uint nodeType() = 0;
 };
 
 class NodeInt: public INode{
@@ -27,15 +38,14 @@ private:
 	T content;
 	NodeInt *parent;
 	std::vector<NodeInt *> children;
+	const uint type = INT;
 public:
-	NodeInt(){ type = INT; };
+	NodeInt(){ };
 	~NodeInt(){};
-	NodeInt(NodeInt *p): parent(p){ type = INT; };
-	NodeInt(NodeInt *p, T c): parent(p), content(c){ type = INT; };
+	NodeInt(NodeInt *p): parent(p){ };
+	NodeInt(NodeInt *p, T c): parent(p), content(c){ };
 
-	int getContent(){
-		return content;
-	};
+	int getContent(){ return content;};
 
 	void setContent(T c){
 		content = c;
@@ -86,8 +96,8 @@ private:
 	typedef float T;
 	T content;
 	NodeFl *parent;
-	std::vector<NodeFl *> children;
 public:
+	std::vector<NodeFl *> children;
 	NodeFl(){ type = FLOAT; };
 	~NodeFl(){};
 	NodeFl(NodeFl *p): parent(p){ type = FLOAT; };
@@ -146,8 +156,8 @@ private:
 	typedef std::string T;
 	T content;
 	NodeStr *parent;
-	std::vector<NodeStr *> children;
 public:
+	std::vector<NodeStr *> children;
 	NodeStr(){ type = STR; };
 	~NodeStr(){};
 	NodeStr(NodeStr *p): parent(p){ type = STR; };
@@ -263,7 +273,98 @@ public:
 	};
 
 };
+/*
+class TreeT{
+	INode *root;
+public:
+	TreeT(){
+		root = new NodeInt((NodeInt*)root, (int)NULL);
+	};
 
+	TreeT(int val){
+		root = new NodeInt((NodeInt*)root, val);
+	};
+
+
+	TreeT(float val){
+		root = new NodeFl((NodeFl*)root, val);
+	};
+
+
+	TreeT(std::string val){
+		root = new NodeStr((NodeStr*)root, val);
+	};
+
+	~TreeT(){
+		delete root;
+	};
+
+	// Appending to the root
+	void appendNode(INode *node){
+		root->addChild(node);
+	};
+
+	// Appending to the node
+	void appendNode(INode &node, INode *child){
+		node.addChild(child);
+	};
+
+	void appendNode(INode *node, INode *child){
+		node->addChild(child);
+	};
+
+	void delNode(auto* node){
+		auto *tnode = node->getParent();
+
+		uint i=0;
+
+		while(i < tnode->getChildreQ()){
+			if(tnode->children[i] == &node){
+				tnode->children.erase(tnode->children[i]);
+				break;
+			}
+			i++;
+		}
+	};
+
+	auto cutNode(auto *node){
+		auto *tnode = node->getParent();
+		auto retNode = NULL;
+
+		uint i=0;
+
+		while(i < tnode->children.size()){
+			if(tnode->children[i] == &node){
+				retNode = tnode->children[i];
+				tnode->children.erase(tnode->children[i]);
+				break;
+			}
+			i++;
+		}
+
+		return retNode;
+	};
+
+	auto *getRoot(){
+		return root;
+	};
+
+	auto visit(auto *pN){
+		auto c = pN->getContent();
+		return c;
+	}
+
+	void traverseTree(auto *pN, void (*F)(auto*)){
+		F(pN);
+		int cq = pN->getChildrenQ();
+		// Job with content
+		for(int i=0; i<cq; i++){
+			traverseTree(pN->getChild(i), F);
+		}
+	};
+
+};
+*/
 template<typename T>
 class Tree{
 	Node<T> *root;
