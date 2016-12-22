d2ed61db1da7a6b4d22d859e321719b987eaad62
Gaspar Vigo <gazpar@localhost.localdomain>
2016-12-22 21:35:20 +0300

    [*] Simplified benchmark code

--- algorithms/sort/runscript.py
+++ algorithms/sort/runscript.py
@@ -1,64 +1,29 @@
 #!/usr/bin/python
 import subprocess
 
+def runBenchmark(power, sortName):
+    print "------------------------------------------"
+    print "| Sort type: " + sortName + " |"
+    print "------------------------------------------"
+    for num in range(5,power):
+        f = pow(2, num)
+        print "Quantity of elements in array: " + str(f)
+        out = subprocess.check_output(["./" + sortName + "/sort", "1", str(f)])
+        lst = out.splitlines(True)
+        s = lst[len(lst)-1]
+        print "Spended for sort " + s[0: len(s)-2] + " ms"
+        print "------------------------------------------"
+
 #Elementary sort
-print "Bubble:"
-for num in range(5,16):
-    f = pow(2, num)
-    print "Quantity of elements in array: " + str(f)
-    out = subprocess.check_output(["./bubble/sort", "1", str(f)])
-    lst = out.splitlines(True)
-    s = lst[len(lst)-1]
-    print "Spended for sort " + s[0: len(s)-2] + " ms"
-    print "------------------------------------------"
+runBenchmark(16, "bubble")
 
-print "Comparison estimation:"
-for num in range(5,16):
-    f = pow(2, num)
-    print "Quantity of elements in array: " + str(f)
-    out = subprocess.check_output(["./comparisonestimation/sort", "1", str(f)])
-    lst = out.splitlines(True)
-    s = lst[len(lst)-1]
-    print "Spended for sort " + s[0: len(s)-2] + " ms"
-    print "------------------------------------------"
+runBenchmark(16, "comparisonestimation")
 
-print "Shell:"
-for num in range(5,17):
-    f = pow(2, num)
-    print "Quantity of elements in array: " + str(f)
-    out = subprocess.check_output(["./shell/sort", "1", str(f)])
-    lst = out.splitlines(True)
-    s = lst[len(lst)-1]
-    print "Spended for sort " + s[0: len(s)-2] + " ms"
-    print "------------------------------------------"  
+runBenchmark(16, "shell")
 
 #Advanced
-print "Merge sort, base variant:"
-for num in range(5,22):
-    f = pow(2, num)
-    print "Quantity of elements in array: " + str(f)
-    out = subprocess.check_output(["./mergebase/sort", "1", str(f)])
-    lst = out.splitlines(True)
-    s = lst[len(lst)-1]
-    print "Spended for sort " + s[0: len(s)-2] + " ms"
-    print "------------------------------------------"
+runBenchmark(22, "mergebase")
 
-print "Binary queue:"
-for num in range(5,22):
-    f = pow(2, num)
-    print "Quantity of elements in array: " + str(f)
-    out = subprocess.check_output(["./binqsort/sort", "1", str(f)])
-    lst = out.splitlines(True)
-    s = lst[len(lst)-1]
-    print "Spended for sort " + s[0: len(s)-2] + " ms"
-    print "------------------------------------------"
+runBenchmark(22, "binqsort")
 
-print "Prior queue:"
-for num in range(5,22):
-    f = pow(2, num)
-    print "Quantity of elements in array: " + str(f)
-    out = subprocess.check_output(["./pqsort/sort", "1", str(f)])
-    lst = out.splitlines(True)
-    s = lst[len(lst)-1]
-    print "Spended for sort " + s[0: len(s)-2] + " ms"
-    print "------------------------------------------"
+runBenchmark(22, "pqsort")
