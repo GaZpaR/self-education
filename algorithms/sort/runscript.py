de9d1e0bc0899c37ceee717de83d7ac7392aafd0
Gaspar Vigo <gazpar@localhost.localdomain>
2016-12-22 22:19:44 +0300

    [*] Changed names of some variables

--- algorithms/sort/runscript.py
+++ algorithms/sort/runscript.py
@@ -5,12 +5,12 @@ def runBenchmark(power, sortName):
     print "------------------------------------------"
     print "| Sort type: " + sortName + " |"
     print "------------------------------------------"
-    for num in range(5,power):
-        f = pow(2, num)
+    for arraySize in range(5,power):
+        f = pow(2, arraySize)
         print "Quantity of elements in array: " + str(f)
         out = subprocess.check_output(["./" + sortName + "/sort", "1", str(f)])
-        lst = out.splitlines(True)
-        s = lst[len(lst)-1]
+        resultList = out.splitlines(True)
+        s = resultList[len(resultList)-1]
         print "Spended for sort " + s[0: len(s)-2] + " ms"
         print "------------------------------------------"
 
